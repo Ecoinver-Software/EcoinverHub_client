431b974914bf437638b6339696e2914b0600f638
Melk236 <162185560+Melk236@users.noreply.github.com>
2025-06-04 09:06:44 +0200

    Changes
    Changes

--- EcoinverHub_client/src/app/components/login/login.component.ts
+++ EcoinverHub_client/src/app/components/login/login.component.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { User } from '../../types/User';
+import { AuthServiceService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login: FormGroup;
+  errorMessage:boolean=false;
+  connectionError:boolean=false;
+  constructor(private authService: AuthServiceService,private ruta:Router
+  ) {
+    this.login = new FormGroup({
+      usuario: new FormControl('', Validators.required),
+      contrasena: new FormControl('', Validators.required)
+    });
+  }
+
+  acceso() {
+    if (this.login.valid) {
+      const user: User = {
+        username: this.login.get('usuario')?.value,
+        email:'',
+        password: this.login.get('contrasena')?.value
+
+      }
+      console.log(user);
+      this.authService.login(user).subscribe(
+        (data) => {
+          console.log(data.token);
+          
+          this.authService.setToken(data.token);
+          this.ruta.navigate(['/home']);
+        },
+        (error) => {
+          console.log(error);
+          this.errorMessage=true;
+          setTimeout(()=>{
+            this.errorMessage=false;
+            alert('Acceso incorrecto');
+          },2500);
+        }
+
+
+      )
+
+    }
+  }
 
 }
---
+++ EcoinverHub_client/src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environment/environment';
+import { User } from '../types/User';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+  url=environment.url + '/api/Auth';
+  constructor(private http:HttpClient) { }
+
+  login(usuario:User):Observable<{token:string,rol:string}>{
+    
+    return this.http.post<{token:string,rol:string}>(`${this.url}/login`, usuario);   
+    
+  }
+  setToken(token:string){
+    localStorage.setItem('jwt',token);
+  }
+
+}
